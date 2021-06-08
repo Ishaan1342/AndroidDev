2a18b17c64309c20ad643cf0c430ac90bba419eb
ishaan1342 <Vespa@321>
2021-06-08 13:10:18 +0530

    Added button click listener for login page

--- app/src/main/java/com/gmail/gillishaan/loginfood/MainActivity.java
+++ app/src/main/java/com/gmail/gillishaan/loginfood/MainActivity.java
@@ -3,12 +3,25 @@ package com.gmail.gillishaan.loginfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btClickMe;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    btClickMe = findViewById(R.id.button);
+        btClickMe.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Next page is coming soon!, We're working on it.", Toast.LENGTH_SHORT).show();
+        }
+    });
     }
 }
